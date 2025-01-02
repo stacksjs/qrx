60fc453d0975370da24eaf6c9f679a6216d3990e
Chris <chrisbreuer93@gmail.com>
2025-01-02 19:23:34 +0100

    chore: wip
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip

---
+++ packages/qar/barcode/renderers/index.ts
@@ -0,0 +1,5 @@
+import CanvasRenderer from './canvas'
+import ObjectRenderer from './object'
+import SVGRenderer from './svg'
+
+export default { CanvasRenderer, SVGRenderer, ObjectRenderer }
---
+++ packages/qar/qrcode/QRMath.ts
@@ -0,0 +1,43 @@
+interface QrMath {
+  glog: (n: number) => number
+  gexp: (n: number) => number
+  EXP_TABLE: number[]
+  LOG_TABLE: number[]
+}
+
+export const QRMath: QrMath = {
+  glog(n: number): number {
+    if (n < 1) {
+      throw new Error(`glog(${n})`)
+    }
+
+    return QRMath.LOG_TABLE[n]
+  },
+
+  gexp(n: number): number {
+    while (n < 0) {
+      n += 255
+    }
+
+    while (n >= 256) {
+      n -= 255
+    }
+
+    return QRMath.EXP_TABLE[n]
+  },
+
+  EXP_TABLE: Array.from({ length: 256 }),
+  LOG_TABLE: Array.from({ length: 256 }),
+}
+
+for (let i = 0; i < 8; i++) {
+  QRMath.EXP_TABLE[i] = 1 << i
+}
+
+for (let i = 8; i < 256; i++) {
+  QRMath.EXP_TABLE[i] = QRMath.EXP_TABLE[i - 4] ^ QRMath.EXP_TABLE[i - 5] ^ QRMath.EXP_TABLE[i - 6] ^ QRMath.EXP_TABLE[i - 8]
+}
+
+for (let i = 0; i < 255; i++) {
+  QRMath.LOG_TABLE[QRMath.EXP_TABLE[i]] = i
+}
