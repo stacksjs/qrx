3c54bd35830f6788fcbc3a2bbb8ee03d62f9abbc
Chris <chrisbreuer93@gmail.com>
2025-01-07 22:33:24 +0100

    chore: wip

--- packages/qrx/src/qrcode/index.ts
+++ packages/qrx/src/qrcode/index.ts
@@ -1,3 +1,7 @@
+import type { QRCodeOptions, RSBlock } from './types'
+import { QRErrorCorrectLevel, QRMaskPattern, QRMode } from './types'
+import { getAndroid, getUTF8Length, isSupportCanvas } from './utils'
+
 /**
  * A TypeScript library for working with QR codes.
  *
@@ -11,88 +15,6 @@
  * References:
  * - https://github.com/stacksjs/qrx
  */
-
-/**
- * Enumeration of QR modes.
- */
-enum QRMode {
-  MODE_NUMBER = 1, // 1 << 0 is equivalent to 1
-  MODE_ALPHA_NUM = 2, // 1 << 1 is equivalent to 2
-  MODE_8BIT_BYTE = 4, // 1 << 2 is equivalent to 4
-  MODE_KANJI = 8, // 1 << 3 is equivalent to 8
-}
-
-/**
- * Enumeration of QR error-correction levels.
- */
-enum QRErrorCorrectLevel {
-  L = 1,
-  M = 0,
-  Q = 3,
-  H = 2,
-}
-
-/**
- * Enumeration of QR masking patterns.
- */
-enum QRMaskPattern {
-  PATTERN000 = 0,
-  PATTERN001 = 1,
-  PATTERN010 = 2,
-  PATTERN011 = 3,
-  PATTERN100 = 4,
-  PATTERN101 = 5,
-  PATTERN110 = 6,
-  PATTERN111 = 7,
-}
-
-/**
- * Helper interface defining the structure of RS Blocks.
- */
-interface RSBlock {
-  totalCount: number
-  dataCount: number
-  errorCount: number
-}
-
-/**
- * Default options for drawing/rendering the QRCode.
- */
-interface QRCodeOptions {
-  text?: string
-  width?: number
-  height?: number
-  colorDark?: string
-  colorLight?: string
-  correctLevel?: QRErrorCorrectLevel
-  useSVG?: boolean // If true, will prefer SVG drawing
-}
-
-/**
- * Helper function to determine if the current browser supports the Canvas API.
- */
-function isSupportCanvas(): boolean {
-  return typeof CanvasRenderingContext2D !== 'undefined'
-}
-
-/**
- * Helper function to detect Android versions (workarounds for older Android).
- */
-function getAndroid(): number | false {
-  const sAgent = navigator.userAgent.toLowerCase()
-
-  if (sAgent.includes('android')) {
-    // Try to grab the version number
-    const match = sAgent.match(/android (\d\.\d)/i)
-    if (match && match[1]) {
-      return Number.parseFloat(match[1])
-    }
-
-    return 1 // No version number found
-  }
-
-  return false
-}
 
 /**
  * A utility class for typical QR math functions, used by polynomial operations.
@@ -1322,14 +1244,6 @@ function getTypeNumber(sText: string, nCorrectLevel: QRErrorCorrectLevel): numbe
     }
   }
   return nType
-}
-
-/**
- * Returns the length of text when encoded in UTF-8, plus BOM if needed.
- */
-function getUTF8Length(sText: string): number {
-  const replacedText = encodeURI(sText).toString().replace(/%[0-9a-f]{2}/gi, 'a')
-  return replacedText.length + (replacedText.length !== sText.length ? 3 : 0)
 }
 
 /**
